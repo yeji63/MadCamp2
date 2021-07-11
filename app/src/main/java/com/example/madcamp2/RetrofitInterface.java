85e6a8d510ea497242668e22831f1d3c881550a7
Seung-an-Jung <qnwktmddks@gmail.com>
2021-07-11 20:42:52 +0900

    img db

--- app/src/main/java/com/example/madcamp2/RetrofitInterface.java
+++ app/src/main/java/com/example/madcamp2/RetrofitInterface.java
@@ -2,10 +2,14 @@ package com.example.madcamp2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface RetrofitInterface {
 
@@ -20,6 +24,9 @@ public interface RetrofitInterface {
 
     @GET("/getgroup")
     Call<ArrayList<Listgroup>> executeGroupGet();
+
+    @POST("/upload")
+    Call<RequestBody> uplaodImage (@Part MultipartBody.Part part, @Part("somedate") RequestBody requestBody);
 
 
 }
